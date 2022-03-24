29106c7c723aca86745f87d3ec8c8d549a30661b
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-03-24 17:17:14 +0800

    fix: esbuild options

--- webpack-esbuild.config.js
+++ webpack-esbuild.config.js
@@ -10,16 +10,6 @@ module.exports = {
     minimizer: [
       new TerserPlugin({
         minify: TerserPlugin.esbuildMinify,
-        // `terserOptions` options will be passed to `esbuild`
-        // Link to options - https://esbuild.github.io/api/#minify
-        // Note: the `minify` options is true by default (and override other `minify*` options), so if you want to disable the `minifyIdentifiers` option (or other `minify*` options) please use:
-        // terserOptions: {
-        //   minify: false,
-        //   minifyWhitespace: true,
-        //   minifyIdentifiers: false,
-        //   minifySyntax: true,
-        // },
-        terserOptions: {},
       }),
     ],
   },
